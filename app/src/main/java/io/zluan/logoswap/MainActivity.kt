9bfd35d73992135744b010cf077b70c58ffcec52
Zhaonian Luan <lzn0830@gmail.com>
2019-09-21 11:39:02 -0700

    Feature/opencv package (#3)
    * import and set up openCV module
    
    * set up openCV and tested by Toast
    
    * added lint issue exception for openCV

--- app/src/main/java/io/zluan/logoswap/MainActivity.kt
+++ app/src/main/java/io/zluan/logoswap/MainActivity.kt
@@ -2,11 +2,19 @@ package io.zluan.logoswap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_LONG).show()
+        }
     }
 }
