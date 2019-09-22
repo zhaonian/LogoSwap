eff9630badc718da3449fedf3428611f934fa2d3
Zhaonian Luan <lzn0830@gmail.com>
2019-09-22 11:32:30 -0700

    Feature/cameraX setup (#4)
    * add/upgrade/format dependecies
    
    * add permission request fragment, and navigation compoenent, and dummy camera fragment
    
    * fix ktlint

---
+++ app/src/main/java/io/zluan/logoswap/ui/CameraFragment.kt
@@ -0,0 +1,19 @@
+package io.zluan.logoswap.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.zluan.logoswap.R
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+}
