eb2e5f4c96f4054c6bed002964ec055f42866d6d
Chifeng Wen <chifenw@google.com>
2019-10-05 12:03:10 -0700

    Merge with the master branch.

--- pixelmod/src/main/java/org/e8yes/pixelmod/repository/ImageDecalRepository.kt
+++ pixelmod/src/main/java/org/e8yes/pixelmod/repository/ImageDecalRepository.kt
@@ -1,13 +1,31 @@
 package org.e8yes.pixelmod.repository
 
+import android.graphics.Bitmap
+import java.io.File
+
+data class ImageDecal(val handle: DecalHandle, val path: String, val image: Bitmap)
+
 /** A collection of decals in the form of physical files. */
-class ImageDecalRepository(private val descriptor: DecalRepositoryDescriptor) : IDecalRepository {
+class ImageDecalRepository : IDecalRepository {
+
+    private val descriptor: DecalRepositoryDescriptor
+    private val decals: List<ImageDecal> = ArrayList()
+
+    constructor(descriptor: DecalRepositoryDescriptor) {
+        this.descriptor = descriptor
+
+        // TODO: Read and parse index file then load in all images in the repository.
+    }
 
     override fun decals(): List<DecalHandle> {
-        TODO("not implemented")
+        return decals.map { decal -> decal.handle }.toList()
     }
 
     override fun descriptor(): DecalRepositoryDescriptor {
         return descriptor
+    }
+
+    fun imageDecal(handle: DecalHandle): ImageDecal? {
+        return decals.findLast { decal -> decal.handle == handle }
     }
 }
